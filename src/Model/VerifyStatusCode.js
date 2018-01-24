cdb4be5ea57fce6a01ac7405719b106214303679
Nordes Lamarre <nordes.menard-lamarre@konicaminolta.com>
2018-01-24 13:15:30 +0900

    First draft with ts + publish

---
+++ src/Model/VerifyStatusCode.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var VerifyStatusCode;
+(function (VerifyStatusCode) {
+    VerifyStatusCode["Unauthorized"] = "Unauthorized";
+    VerifyStatusCode["Authorized"] = "Authorized";
+    VerifyStatusCode["Error"] = "Error";
+})(VerifyStatusCode = exports.VerifyStatusCode || (exports.VerifyStatusCode = {}));
---
+++ src/OidcValidator.d.ts
@@ -0,0 +1,48 @@
+import { ValidatorResult, VerifyOptions } from "./Model";
+/**
+ * Class used to validate the OIDC JWT Token.
+ */
+export declare class OidcValidator {
+    private oidcDiscoveryUri;
+    private publicKey;
+    /**
+     * Create an instance of the OIDC Validator
+     * @param options Verify options containing the 'Issuer'
+     */
+    constructor(options: VerifyOptions);
+    /**
+     * OidcDiscovery URI
+     */
+    readonly OidcDiscoveryUri: string;
+    /**
+     * Verify the JWT against the OID issuer.
+     * @param token JWT Token coming from an Idenitty Server
+     */
+    verify(token: string): Promise<ValidatorResult>;
+    /**
+     * Validate the JWT token.
+     * @param token The JWT token
+     * @param publicKey Public key used to certify the token
+     */
+    private jwtVerify(token, publicKey);
+    /**
+     * Format the certificate
+     * @param cert Certificate (public key)
+     */
+    private formatCertificate(cert);
+    /**
+     * Fetch the discoery Jwk URI(s)
+     */
+    private FetchDiscoveryJwkUris();
+    /**
+     * Fetch the JWK first X509 Certificate
+     * @param jwksUri Uri where to get the certificates
+     */
+    private FetchJwkFirstX5C(jwksUri);
+    /**
+     * Save the certificate and then validate the token against the certificate.
+     * @param x5c Certificate
+     * @param token OIDC Token
+     */
+    private SaveCertificateAndCheck(x5c, token);
+}
